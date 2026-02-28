442c74887f2cab7fa4b27a925aede5a438a77d4e
Moisarm <moisarm03@gmail.com>
2026-02-28 19:36:41 -0400

    [Feature] (server and db) :
    - Set up Express server with basic middlewares (CORS, logging, JSON parsing, cookie parsing).
    
    -Set up PrismaORM with the schema and config
    
    - Add necessary dependencies for Express, Prisma, and related tools.
    
    -Also created a file for env reading

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { config } from "./src/infrastructure/config/server/variables.config";
+import { cors_options } from "./src/infrastructure/config/server/cors.config";
+
+const server = express();
+
+//Middlewares
+server.use(cors(cors_options));
+server.use(morgan("tiny"));
+server.use(express.json());
+server.use(cookieParser());
+
+server.use("/api", () => {});
+
+server.listen(config.PORT, () => {
+  console.log(`Server running on http://localhost:${config.PORT}`);
+});
