143c27c5dd097db4b6d0c8ac50337a7643888416
Moisarm <moisarm03@gmail.com>
2026-02-28 23:28:27 -0400

    [Feature] (Config) :
     - Add cors config, variables config  and allowed the use of decorators on tsconfig

---
+++ backend/src/infrastructure/config/server/cors.config.ts
@@ -0,0 +1,6 @@
+export const cors_options = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
---
+++ backend/src/infrastructure/config/server/variables.config.ts
@@ -0,0 +1,4 @@
+export const config = {
+  PORT: process.env.PORT || "3002",
+  SECRET: process.env.SECRET || "This_is_Very_very_very_Secret",
+};
