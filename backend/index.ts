e3cf6db380f46e633e5df8642cc6700eb9b1aa25
Moisarm <moisarm03@gmail.com>
2026-03-01 04:38:33 -0400

    [Feature] (Server) :
    - added the index router and not found handler to main file

--- backend/index.ts
+++ backend/index.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { config } from "./src/infrastructure/config/server/variables.config";
 import { cors_options } from "./src/infrastructure/config/server/cors.config";
+import { index_router } from "./src/presentation/routes/index.route";
+import { not_found_handler } from "./src/presentation/middlewares/server/404-handler";
 
 const server = express();
 
@@ -13,8 +15,10 @@ server.use(morgan("tiny"));
 server.use(express.json());
 server.use(cookieParser());
 
-server.use("/api", () => {});
+server.use("/api", index_router);
 
 server.listen(config.PORT, () => {
   console.log(`Server running on http://localhost:${config.PORT}`);
 });
+
+server.use(not_found_handler);
